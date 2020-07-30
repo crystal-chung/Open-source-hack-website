fb2182d7c05d90a15b5c617a2ceea524f80d5e8f
Crystal Chung <crystal_chung@intuit.com>
2020-07-30 09:20:06 -0700

    initial twitterstream

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Open-source-hack-website
+# opensource-social-wall
+Opensource Social Wall Application
+
+Getting started
+===============
+1. git clone git@https://github.com/crystal-chung/opensource-social-wall.git
+2. cd opensource-social-wall
+3. npm install
+4. node index.js
+5. Open http://localhost:{port}/
--- index.js
+++ index.js
@@ -1,10 +1,50 @@
-const express = require('express')
+var Twit = require('twit');
+const express = require('express');
 const app = express();
+const server = require('http').Server(app);
+var io = require('socket.io')(server);
+var fs = require('fs');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('client'));
+
+io.on('connection', function(socket) {
+    T.get('search/tweets', { q: '#gracehopper', count: 100 }, function(err, data, response) {
+      var tweetArray=[];
+        for (let index = 0; index < data.statuses.length; index++) {
+            const tweet = data.statuses[index];
+            var tweetbody = {
+              'text': tweet.text,
+              'userScreenName': "@" + tweet.user.screen_name,
+              'userImage': tweet.user.profile_image_url_https,
+              'userDescription': tweet.user.description,
+            }
+            try {
+              if(tweet.entities.media[0].media_url_https) {
+                tweetbody['image'] = tweet.entities.media[0].media_url_https;
+              }
+            } catch(err) { }
+            tweetArray.push(tweetbody);
+        }     
+        io.emit('allTweet',tweetArray)
+    })
+
+    var stream = T.stream('statuses/filter', { track: '#gracehopper', language: 'en' })
+
+    stream.on('tweet', function (tweet) {
+        io.emit('tweet',{ 'tweet': tweet });
+    })
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+var T = new Twit({
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+});
+
+// listen for requests
+const listener = server.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
